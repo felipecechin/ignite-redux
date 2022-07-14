cf74090f78395215eccae38eaaf44703114ee999
Felipe Cechin <ficechin@hotmail.com>
2022-07-14 17:50:29 -0300

    add product to cart reducer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,13 +1,47 @@
 import { Reducer } from "@reduxjs/toolkit";
+import produce from "immer";
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
     items: []
 }
 
-const cart: Reducer<ICartState> = (state, action) => {
-    console.log(state, action)
-    return INITIAL_STATE;
+const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
+    return produce(state, draft => {
+        switch (action.type) {
+            case 'ADD_PRODUCT_TO_CART': {
+                const { product } = action.payload;
+
+                const productInCartIndex = draft.items.findIndex(item =>
+                    item.product.id === product.id
+                )
+
+                if (productInCartIndex >= 0) {
+                    draft.items[productInCartIndex].quantity++;
+                } else {
+                    draft.items.push({
+                        product,
+                        quantity: 1
+                    })
+                }
+
+                return draft;
+                // return {
+                //     ...state,
+                //     items: [
+                //         ...state.items,
+                //         {
+                //             product,
+                //             quantity: 1
+                //         }
+                //     ]
+                // };
+            }
+            default: {
+                return draft;
+            }
+        }
+    })
 }
 
 export default cart;
